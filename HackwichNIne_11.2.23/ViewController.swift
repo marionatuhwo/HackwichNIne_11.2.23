2a1b126a985d7eb5b17595840b80508027aa8d03
marionatuhwo <ano@hawaii.edu>
2023-11-02 09:58:26 -1000

    Completed Parts 1 of Hackwich 9

--- HackwichNIne_11.2.23/ViewController.swift
+++ HackwichNIne_11.2.23/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var candyArray = ["Reese's Peanut Butter Cups - Pumpkin", "Snickers Candy Bar", "Kit Kat Chocolate Wafer", "Twix"]
+        
+        var ratingsArray = ["⭐️⭐️⭐️⭐️⭐️", "⭐️⭐️⭐️⭐️⭐️", "⭐️⭐️⭐️⭐️", "⭐️⭐️⭐️"]
+
+        //Part 8
+        var categoryOneImagesData = [String]()
+        
+        override func viewDidLoad()
+            {
+                super.viewDidLoad()
+                //Part 8
+                /*let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+                let dict = NSDictionary(contentsOfFile: path!)
+                categoryOneImagesData = dict!.object(forKey:"CategoryOneImages") as! [String]*/
+            }
+        
+            
+            @IBOutlet weak var tableView: UITableView!
+            
+            func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+            {
+                return candyArray.count
+            }
+            
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->   UITableViewCell
+    {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+                let text = candyArray[indexPath.row]
+                cell.detailTextLabel?.text = ratingsArray[indexPath.row]
+                cell.textLabel?.text = text
+                return cell
+            }
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+            {
+                tableView.deselectRow(at: indexPath, animated: true)
+            }
+
+        //Part 9
+        
+       /* override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "mySegue"
+            {
+                let s1 = segue.destination as! secondDetailViewController
+                let imageIndex = tableView.indexPathForSelectedRow?.row
+                s1.imagePass = categoryOneImagesData[imageIndex!]
+            }
+        }*/
+
     }
 
-
-}
 
---
+++ HackwichNIne_11.2.23/secondDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  secondDetailViewController.swift
+//  HackwichNIne_11.2.23
+//
+//  Created by Marion Ano on 11/2/23.
+//
+
+import UIKit
+
+class secondDetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
